4c7b225c0395a6f4498ad522f9097b9eec11e6af
geraltIV <myroslav.ivaskevych92@gmail.com>
2020-07-16 21:06:54 +0300

    implemented a simple currency converter using XMLHttpRequest; gitignore file changed

---
+++ currency-converter/js/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const inputUAH = document.querySelector('#uah'),
+      inputUSD = document.querySelector('#usd');
+
+inputUAH.addEventListener('input', () => {
+    const request = new XMLHttpRequest();
+
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+    
+    request.addEventListener('load', () => {
+        if (request.status === 200) {
+            console.log(request.response);
+            const data = JSON.parse(request.response);
+            console.log(data.current.usd);
+            inputUSD.value = (+inputUAH.value / data.current.usd).toFixed(2);
+        } else {
+            inputUSD.value = 'Something goes bad';
+        }
+    });
+});
