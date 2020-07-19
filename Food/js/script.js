de0249eb4aa7db956e4e8a3b3dd8c62f31687cce
geraltIV <myroslav.ivaskevych92@gmail.com>
2020-07-19 14:58:45 +0300

    sending data to the server using fetch and promise

--- Food/js/script.js
+++ Food/js/script.js
@@ -242,10 +242,6 @@ window.addEventListener('DOMContentLoaded', () => {
             `;
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-            request.setRequestHeader('Content-type', 'application/json');
-
             const formData = new FormData(form);
 
             const obj = {};
@@ -253,19 +249,23 @@ window.addEventListener('DOMContentLoaded', () => {
                 obj[key] = value;
             });
 
-            const json = JSON.stringify(obj);
-
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-                    statusMessage.remove();
-                } else {
-                    showThanksModal(message.failure);
-                }
+            fetch('server.php', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                // body: formData
+                body: JSON.stringify(obj)
+            })
+            .then(data => data.text())
+            .then((data) => {
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
             })
         })
     }
