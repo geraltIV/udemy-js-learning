5a2c9fba4da7b7ce5864e932014eb4a597beddaa
geraltIV <myroslav.ivaskevych92@gmail.com>
2020-07-21 19:27:47 +0300

    used regexp in project

--- Food/js/script.js
+++ Food/js/script.js
@@ -392,7 +392,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     slidesField.style.width = 100 * slides.length + '%';
     slidesField.style.display = 'flex';
-    slidesField.style.transition = '.4s all cubic-bezier(.27,.73,.69,.28)';
+    slidesField.style.transition = '.4s all ease-in-out';
 
     slidesWarpper.style.overflow = 'hidden';
 
@@ -434,11 +434,15 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    function deleteNotDigits(string) {
+        return +string.replace(/\D/g, '');
+    }
+
     next.addEventListener('click', () => {
-        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+        if (offset == deleteNotDigits(width) * (slides.length - 1)) {
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2)
+            offset += deleteNotDigits(width);
         }
         slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -455,9 +459,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
     prev.addEventListener('click', () => {
         if (offset == 0) {
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = deleteNotDigits(width) * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2)
+            offset -= deleteNotDigits(width);
         }
         slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -477,7 +481,7 @@ window.addEventListener('DOMContentLoaded', () => {
             const slideTo = event.target.getAttribute('data-slide-to');
 
             slideIndex = slideTo;
-            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            offset = deleteNotDigits(width) * (slideTo - 1);
             slidesField.style.transform = `translateX(-${offset}px)`;
 
             setCurrentSliderNumber(slides, current, slideIndex);
