b2a3e0e7c8c88fcd76de7a754945ecf4c11c5d85
geraltIV <myroslav.ivaskevych92@gmail.com>
2020-07-21 16:41:13 +0300

    implemented carousel indicators; code improvement

--- Food/js/script.js
+++ Food/js/script.js
@@ -327,6 +327,7 @@ window.addEventListener('DOMContentLoaded', () => {
     //Slider
 
     const slides = document.querySelectorAll('.offer__slide'),
+          slider = document.querySelector('.offer__slider'),
           prev = document.querySelector('.offer__slider-prev'),
           next = document.querySelector('.offer__slider-next'),
           total = document.querySelector('#total'),
@@ -399,6 +400,40 @@ window.addEventListener('DOMContentLoaded', () => {
         item.style.width = width;
     });
 
+    slider.style.position = 'relative';
+
+    const indicators = document.createElement('ol'),
+          dots = [];
+    indicators.classList.add('carousel-indicators');
+
+    slider.append(indicators);
+
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i + 1);
+        dot.classList.add('dot');
+
+        if (i == 0) {
+            dot.style.opacity = 1;
+        }
+
+        indicators.append(dot);
+        dots.push(dot);
+    };
+
+    function setActiveDots(dotsArr) {
+        dotsArr.forEach(item => item.style.opacity = '.5');
+        dots[slideIndex - 1].style.opacity = '1';
+    };
+
+    function setCurrentSliderNumber(slidesArr, currentNum, index) {
+        if (slidesArr.length < 10) {
+            currentNum.textContent = `0${index}`;
+        } else {
+            currentNum.textContent = slideIndex;
+        }
+    }
+
     next.addEventListener('click', () => {
         if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
             offset = 0;
@@ -413,11 +448,9 @@ window.addEventListener('DOMContentLoaded', () => {
             slideIndex++;
         }
 
-        if (slides.length < 10) {
-            current.textContent = `0${slideIndex}`;
-        } else {
-            current.textContent = slideIndex;
-        }
+        setCurrentSliderNumber(slides, current, slideIndex);
+
+        setActiveDots(dots);
     });
 
     prev.addEventListener('click', () => {
@@ -434,10 +467,22 @@ window.addEventListener('DOMContentLoaded', () => {
             slideIndex--;
         }
 
-        if (slides.length < 10) {
-            current.textContent = `0${slideIndex}`;
-        } else {
-            current.textContent = slideIndex;
-        }
+        setCurrentSliderNumber(slides, current, slideIndex);
+
+        setActiveDots(dots);
     });
+
+    dots.forEach(dot => {
+        dot.addEventListener('click', (event) => {
+            const slideTo = event.target.getAttribute('data-slide-to');
+
+            slideIndex = slideTo;
+            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            slidesField.style.transform = `translateX(-${offset}px)`;
+
+            setCurrentSliderNumber(slides, current, slideIndex);
+
+            setActiveDots(dots);
+        })
+    })
 });
